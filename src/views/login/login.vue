415ca2c8c9b0e31e530f9f44de906033dd0c7b8a
rainicec <846287612@qq.com>
2019-06-18 21:00:19 +0800

    登录页：登录验证

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,10 +1,21 @@
 <template>
   <div>
     <div id="logo-box">
-      <img src="//gw.alicdn.com/tfs/TB1QfeUQAvoK1RjSZFDXXXY3pXa-300-138.png" class="logo">
+      <img
+        src="//gw.alicdn.com/tfs/TB1QfeUQAvoK1RjSZFDXXXY3pXa-300-138.png"
+        class="logo"
+      />
     </div>
-    <div id="mini-login-body" scroll="no" class="damai lang-zh_CN mobile theme-default" data-spm>
-      <div id="login" class="width-vertical login-label-text login-view-sms damai">
+    <div
+      id="mini-login-body"
+      scroll="no"
+      class="damai lang-zh_CN mobile theme-default"
+      data-spm
+    >
+      <div
+        id="login"
+        class="width-vertical login-label-text login-view-sms damai"
+      >
         <div class="master-login-title">短信登录</div>
         <div class="login-content nc-outer-box">
           <div class="login-sms">
@@ -41,22 +52,33 @@
                               <div
                                 class="picker-item picker-item-selected"
                                 style="height: 36px; line-height: 36px;"
-                              >+86 中国大陆</div>
+                              >
+                                +86 中国大陆
+                              </div>
                               <div
                                 class="picker-item"
                                 style="height: 36px; line-height: 36px;"
-                              >+852 中国香港</div>
+                              >
+                                +852 中国香港
+                              </div>
                               <div
                                 class="picker-item"
                                 style="height: 36px; line-height: 36px;"
-                              >+853 中国澳门</div>
+                              >
+                                +853 中国澳门
+                              </div>
                               <div
                                 class="picker-item"
                                 style="height: 36px; line-height: 36px;"
-                              >+886 中国台湾地区</div>
+                              >
+                                +886 中国台湾地区
+                              </div>
                             </div>
                           </div>
-                          <div class="picker-highlight" style="height: 36px; margin-top: -18px;"></div>
+                          <div
+                            class="picker-highlight"
+                            style="height: 36px; margin-top: -18px;"
+                          ></div>
                         </div>
                       </div>
                     </div>
@@ -70,7 +92,8 @@
                     tabindex="1"
                     placeholder="请输入手机号"
                     autocapitalize="off"
-                  >
+                    v-model="phone"
+                  />
                   <div class="underline">
                     <div class="unfocused-line"></div>
                     <div class="focused-line"></div>
@@ -94,14 +117,17 @@
                     maxlength="6"
                     autocomplete="off"
                     autocapitalize="off"
-                  >
+                    v-model="incode"
+                  />
                   <div class="underline">
                     <div class="unfocused-line"></div>
                     <div class="focused-line"></div>
                   </div>
                 </div>
                 <div class="send-btn">
-                  <a href="#" class="send-btn-link">获取验证码</a>
+                  <a href="#" class="send-btn-link" @click="getcode"
+                    >获取验证码</a
+                  >
                 </div>
               </div>
               <div>
@@ -115,13 +141,22 @@
                     <div id="nc_1_n1t" class="nc_scale">
                       <div id="nc_1__bg" class="nc_bg"></div>
                       <span id="nc_1_n1z" class="nc_iconfont btn_slide"></span>
-                      <div id="nc_1__scale_text" class="scale_text slidetounlock">
-                        <span class="nc-lang-cnt" data-nc-lang="_startTEXT">请按住滑块，拖动到最右边</span>
+                      <div
+                        id="nc_1__scale_text"
+                        class="scale_text slidetounlock"
+                      >
+                        <span class="nc-lang-cnt" data-nc-lang="_startTEXT"
+                          >请按住滑块，拖动到最右边</span
+                        >
                       </div>
                       <div id="nc_1_clickCaptcha" class="clickCaptcha">
                         <div class="clickCaptcha_text">
                           <b id="nc_1__captcha_text" class="nc_captch_text"></b>
-                          <i id="nc_1__btn_2" class="nc_iconfont nc_btn_2 btn_refresh"></i>
+                          <i
+                            id="nc_1__btn_2"
+                            class="nc_iconfont nc_btn_2 btn_refresh"
+                            ></i
+                          >
                         </div>
                         <div class="clickCaptcha_img"></div>
                         <div class="clickCaptcha_btn"></div>
@@ -133,17 +168,27 @@
                             maxlength="6"
                             type="text"
                             style="ime-mode:disabled"
-                          >
+                          />
                         </div>
-                        <div class="imgCaptcha_img" id="nc_1__imgCaptcha_img"></div>
+                        <div
+                          class="imgCaptcha_img"
+                          id="nc_1__imgCaptcha_img"
+                        ></div>
                         <i
                           id="nc_1__btn_1"
                           class="nc_iconfont nc_btn_1 btn_refresh"
                           onclick="document.getElementById('nc_1__imgCaptcha_img').children[0].click()"
-                        ></i>
+                          ></i
+                        >
                         <div class="imgCaptcha_btn">
-                          <div id="nc_1__captcha_img_text" class="nc_captcha_img_text"></div>
-                          <div id="nc_1_scale_submit" class="nc_scale_submit"></div>
+                          <div
+                            id="nc_1__captcha_img_text"
+                            class="nc_captcha_img_text"
+                          ></div>
+                          <div
+                            id="nc_1_scale_submit"
+                            class="nc_scale_submit"
+                          ></div>
                         </div>
                       </div>
                       <div id="nc_1_cc" class="nc-cc"></div>
@@ -153,9 +198,12 @@
                         role="button"
                         class="nc_voicebtn nc_iconfont"
                         style="display:none"
-                      ></i>
+                        ></i
+                      >
                       <b id="nc_1__helpbtn" class="nc_helpbtn">
-                        <span class="nc-lang-cnt" data-nc-lang="_learning">了解新功能</span>
+                        <span class="nc-lang-cnt" data-nc-lang="_learning"
+                          >了解新功能</span
+                        >
                       </b>
                     </div>
                     <div id="nc_1__voice" class="nc_voice"></div>
@@ -163,25 +211,104 @@
                 </div>
               </div>
               <div class="fm-btn">
-                <button type="submit" tabindex="3" class="fm-button fm-submit sms-login">登录</button>
+                <div
+                  class="fm-button fm-submit sms-login"
+                  @click="checkall"
+                  style="text-align: center"
+                >
+                  登录
+                </div>
               </div>
               <div class="login-blocks login-links">
-                <a href="#" target="_self" class="password-login-link">账号密码登录</a>
+                <a href="#" target="_self" class="password-login-link"
+                  >账号密码登录</a
+                >
                 <a href="#" target="_top" class="register-link">新用户注册</a>
               </div>
             </form>
           </div>
         </div>
       </div>
-      <div id="_umfp" style="display:inline;width:1px;height:1px;overflow:hidden">
+      <div
+        id="_umfp"
+        style="display:inline;width:1px;height:1px;overflow:hidden"
+      >
         <!-- umidServer已经带了/ -->
         <img
           src="https://ynuf.alipay.com/service/clear.png?xt=950e11ec8e442a589fd20b7a1c2c7267b59fe340&amp;xa=090D1F110F1878292829161C"
-        >
+        />
       </div>
+      <div v-text="tip" class="tip" v-show="showtip"></div>
     </div>
   </div>
 </template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      phone: "",
+      code: "h",
+      incode: "",
+      tip: "手机号格式不正确",
+      phoneok: 0,
+      codeok: 0,
+      showtip: 0
+    };
+  },
+  methods: {
+    getcode() {
+      let html =
+        "0987654321qwertyuioplkjhgfdsazxcvbnmZXCVBNMLKJHGFDSAQWERTYUIOP";
+      let res = "";
+      for (let i = 0; i < 4; i++) {
+        let now = parseInt(Math.random() * html.length);
+        res += html[now];
+      }
+      this.code = res;
+      console.log(this.code);
+    },
+    checkphone() {
+      let reg = /^1[3-9]\d{9}$/;
+      if (reg.test(this.phone)) {
+        this.phoneok = 1;
+      } else {
+        this.phoneok = 0;
+        this.tip = "手机号格式不正确"
+        this.showtip = 1;
+        setTimeout(() => {
+          this.showtip = 0;
+        }, 2000);
+      }
+    },
+    checkcode() {
+      if (this.phoneok) {
+        if (this.code.toLowerCase() === this.incode) {
+          this.codeok = 1;
+        } else {
+          this.codeok = 0;
+          this.tip = "验证码不正确";
+          this.showtip = 1;
+          setTimeout(() => {
+            this.showtip = 0;
+          }, 2000);
+        }
+      }
+    },
+    checkall() {
+      this.checkphone();
+      this.checkcode();
+      if (this.phoneok && this.codeok) {
+        this.$router.push("/mine");
+        let now = new Date();
+        now.setDate(now.getDate() + 7);
+        document.cookie = "uname=" + this.phone + ";expires=" + now + ";path=/";
+      }
+    }
+  }
+});
+</script>
+
 
 <style lang="less" scoped>
 @import "../../assets/common/login.css";
@@ -194,6 +321,19 @@
     height: 1.21333rem;
     margin-top: 1.02667rem;
   }
+}
+.tip {
+  width: 4rem;
+  height: 1.333333rem;
+  background: rgba(0, 0, 0, 0.6);
+  color: #fff;
+  font-size: 0.4rem;
+  line-height: 1.333333rem;
+  text-align: center;
+  position: fixed;
+  top: 50%;
+  right: 50%;
+  transform: translateX(50%);
 }
 </style>
 
